b88c1782c0880e407d62c50c291b9b3374935b19
rahimshahad <rahim.shahad2@gmail.com>
2021-01-01 22:47:06 -0500

    commit tree e41e5c0113d05a1600d6a6b67da60309d0fc4625

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    def initialize(roster)
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+    def add_student(name, grade)
+        if (roster[grade])
+            roster[grade] << name
+        else
+             roster[grade] = []
+             roster[grade] << name
+        end
+    end
+    def grade(grade)
+        roster[grade]
+    end
+    def sort
+        roster.map do |key, value|
+            value.sort!
+        end
+        roster.sort.to_h
+    end
+    
+end
